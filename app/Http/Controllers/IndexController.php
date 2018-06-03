87e7f63bc97afbc1c1c51a4339e09f02a7126434
Yingxiangempire <yingxiangempire@outlook.com>
2018-06-03 13:55:03 +0800

    cookie

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -8,14 +8,13 @@
 
 namespace App\Http\Controllers;
 use Symfony\Component\HttpFoundation\Cookie as SCookie;
-
 class IndexController extends Controller
 {
     public function index()
     {
         \Illuminate\Support\Facades\View::addExtension('html', 'php');
 
-        return response()->view('index')->withCookie(new SCookie('sid', 'sid9999', time()+3600));
+        return response(view('index'))->withCookie(new SCookie('sid', 'sid9999', time()+3600));
     }
     
 }
